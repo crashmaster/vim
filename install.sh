ab620b3ec87dd393b0234e475357b7e5d86cb77e
Johnny Crash <cannon.imus@gmail.com>
2013-01-21 17:04:30 +0100

    Added uninstall, do some further testing!

--- install.sh
+++ install.sh
@@ -6,52 +6,64 @@ REPO_BASE=`dirname $0`
 PRINTF=/usr/bin/printf
 EXPR=/usr/bin/expr
 MKDIR=/bin/mkdir
+RMDIR=/bin/rmdir
 CP=/bin/cp
+RM=/bin/rm
 DIFF=/usr/bin/diff
+LS=/bin/ls
+WC=/usr/bin/wc
 
+# Check used tools for their availability
 [ ! -x $PRINTF ] && echo "$PRINTF not found" && exit 1
 [ ! -x $EXPR ] && echo "$EXPR not found" && exit 1
 [ ! -x $MKDIR ] && echo "$MKDIR not found" && exit 1
+[ ! -x $RMDIR ] && echo "$RMDIR not found" && exit 1
 [ ! -x $CP ] && echo "$CP not found" && exit 1
+[ ! -x $RM ] && echo "$RM not found" && exit 1
 [ ! -x $DIFF ] && echo "$DIFF not found" && exit 1
+[ ! -x $LS ] && echo "$LS not found" && exit 1
+[ ! -x $WC ] && echo "$WC not found" && exit 1
 
 usage() {
     $PRINTF "usage: $SCRIPT_NAME [-h|-i|-u]\n\n"
     $PRINTF "Installs the Crashmaster Vim Configuration o_O\n"
 }
 
+error_result() {
+    $PRINTF "%*s\n" $1 "[ERROR]"
+    exit 1
+}
+
 install() {
     local result_indent=0
-    local repo_rel_path=""
     local target_path=""
 
     for i in `find $REPO_BASE/vim*`
     do
-        repo_rel_path=`echo $i | sed -n 's|.*/\(vim.*\)|\1|p'`
-        target_path=~/.$repo_rel_path
-        result_indent=`$EXPR 72 - \( 2 \* ${#repo_rel_path} \)`
-        $PRINTF "repo::%s -> %s" "$repo_rel_path" "$target_path"
+        target_path=~/.`echo $i | sed -n 's|.*/\(vim.*\)|\1|p'`
+        result_indent=`$EXPR 72 - ${#target_path}`
+        $PRINTF "  -> %s" "$target_path"
 
         # directories
         if [ -d $i -a ! -d $target_path ]
         then
-            $MKDIR -p $target_path || exit 1
+            $MKDIR -p $target_path || error_result $result_indent
             $PRINTF "%*s\n" $result_indent "[MKDIR]"
         elif [ -d $i -a -d $target_path ]
         then
-            $PRINTF "%*s\n" $result_indent "[SKIP]"
+            $PRINTF "%*s\n" $result_indent "[EXISTS]"
         fi
 
         # files
         if [ -f $i -a ! -f $target_path ]
         then
-            $CP $i $target_path || exit 1
+            $CP $i $target_path || error_result $result_indent
             $PRINTF "%*s\n" $result_indent "[COPY]"
         elif [ -f $i -a -f $target_path ]
         then
             if $DIFF -u $target_path $i > /dev/null 2>&1
             then
-                $PRINTF "%*s\n" $result_indent "[SKIP]"
+                $PRINTF "%*s\n" $result_indent "[SAME]"
                 continue
             fi
             while true
@@ -65,13 +77,13 @@ install() {
                         $DIFF -u $target_path $i
                         ;;
                     o)
-                        $PRINTF "repo::%s -> %s" "$repo_rel_path" "$target_path"
-                        $CP $i $target_path || exit 1
+                        $PRINTF "  -> %s" "$target_path"
+                        $CP $i $target_path || error_result $result_indent
                         $PRINTF "%*s\n" $result_indent "[COPY]"
                         break
                         ;;
                     s)
-                        $PRINTF "repo::%s -> %s" "$repo_rel_path" "$target_path"
+                        $PRINTF "  -> %s" "$target_path"
                         $PRINTF "%*s\n" $result_indent "[SKIP]"
                         break
                         ;;
@@ -88,7 +100,59 @@ install() {
 }
 
 uninstall() {
-    $PRINTF "Muu!\n"
+    local result_indent=0
+    local repo_path=""
+    local target_path=""
+
+    while true
+    do
+        read -n 1 -p "Really (u)ninstall or (q)uit? " tmp
+        case $tmp in
+            u)
+                $PRINTF "\n"
+                break
+                ;;
+            q)
+                $PRINTF "\n"
+                exit 0
+                ;;
+            *)
+                $PRINTF "\n  %s: incorrect choice\n" $tmp
+                ;;
+        esac
+    done
+
+    # files
+    for i in `find $REPO_BASE/vim*`
+    do
+        [ ! -f $i ] && continue
+        repo_path=`echo $i | sed -n 's|.*/\(vim.*\)|\1|p'`
+        target_path=~/.$repo_path
+        result_indent=`$EXPR 72 - ${#target_path}`
+        if [ -f $target_path ]
+        then
+            $PRINTF "  -> %s" "$target_path"
+            $RM $target_path || error_result $result_indent
+            $PRINTF "%*s\n" $result_indent "[RM]"
+        fi
+    done
+
+    # directories
+    for i in `find $REPO_BASE/vim*`
+    do
+        [ ! -d $i ] && continue
+        repo_path=`echo $i | sed -n 's|.*/\(vim.*\)|\1|p'`
+        target_path=~/.$repo_path
+        result_indent=`$EXPR 72 - ${#target_path}`
+        $PRINTF "  -> %s" "$target_path"
+        if [ `ls -a $target_path | wc -l` -lt 3 ]
+        then
+            $RMDIR $target_path || error_result $result_indent
+            $PRINTF "%*s\n" $result_indent "[RMDIR]"
+        else
+            $PRINTF "%*s\n" $result_indent "[NOT EMPTY]"
+        fi
+    done
 }
 
 case "$@" in
@@ -99,7 +163,6 @@ case "$@" in
         install
         ;;
     -u|--uninstall)
-        # TODO
         uninstall
         ;;
     *)
